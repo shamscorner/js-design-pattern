4fb733b2d0dabb271201244f6c0840c34ba02795
Shamim Hossain <shossain@secureship.ca>
2024-01-27 14:59:54 +0600

    chore: re-recorded version of command pattern

--- command-pattern/index-command.js
+++ command-pattern/index-command.js
@@ -1,3 +1,4 @@
+// file: EmailManager.js
 class EmailManager {
   constructor() {
     this.emails = []; // queue***
@@ -8,12 +9,14 @@ class EmailManager {
   }
 }
 
+// file: Command.js
 class Command {
   constructor(execute) {
     this.execute = execute;
   }
 }
 
+// file: utils.js
 function overflowEmail(length) {
   // limit 10
   const emailLimit = 5;
@@ -29,7 +32,8 @@ function overflowEmail(length) {
   };
 }
 
-function SendEmailCommand(email, id) {
+// file: SendEmailCommand.js
+function sendEmailCommand(email, id) {
   return new Command((emails) => {
     const { successful, message } = overflowEmail(emails.length);
 
@@ -40,19 +44,81 @@ function SendEmailCommand(email, id) {
   });
 }
 
+// file: TrackEmailCommand.js
+function trackEmailCommand(id) {
+  return new Command((emails) => {
+    const isEmailExist = emails.find((emailId) => emailId === id);
+
+    if (!isEmailExist)
+      return {
+        successful: false,
+        message: `Your email with the id - ${id} is not found!`,
+      };
+
+    return {
+      successful: true,
+      message: `Your email had sent a while ago with the id - ${id}.`,
+    };
+  });
+}
+
+// file: DispatchEmailCommand.js
+function dispatchEmailCommand() {
+  return new Command((emails) => {
+    if (!emails.length) return `Your email queue is empty now.`;
+
+    const dispatchedEmailId = emails.shift();
+    return `Your email has been dispatched - ${dispatchedEmailId}`;
+  });
+}
+
+// file: DispatchEmailCommand.js
+function dispatchEmailByIdCommand(id, manager) {
+  return new Command((emails) => {
+    const { successful, message } = manager.execute(trackEmailCommand(id));
+
+    if (!successful) return message;
+
+    const index = emails.indexOf(id);
+    emails.splice(index, 1);
+    return `Your email has been dispatched - ${id}`;
+  });
+}
+
+// file: CancelEmailCommand.js
+// function cancelEmailCommand(id) {
+// 	return new Command((emails) => {
+// 		// cancel email implementation
+// 	});
+// }
+
 // For testing
 const manager = new EmailManager();
 
 console.log(manager.emails);
 
-console.log(manager.execute(SendEmailCommand("Hi! I am John.", "123")));
-console.log(manager.execute(SendEmailCommand("Hi! I am Alex.", "124")));
-console.log(manager.execute(SendEmailCommand("Hi! I am Macy.", "125")));
-console.log(manager.execute(SendEmailCommand("Hi! I am Liza.", "126")));
-console.log(manager.execute(SendEmailCommand("Hi! I am Sharif.", "127")));
-console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
-console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
-console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
-console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
+console.log(manager.execute(sendEmailCommand("Hi! I am John.", "123")));
+console.log(manager.execute(sendEmailCommand("Hi! I am Alex.", "124")));
+console.log(manager.execute(sendEmailCommand("Hi! I am Macy.", "125")));
+// console.log(manager.execute(sendEmailCommand("Hi! I am Liza.", "126")));
+// console.log(manager.execute(sendEmailCommand("Hi! I am Sharif.", "127")));
+// console.log(manager.execute(sendEmailCommand("Hi! I am Masud.", "128")));
+// console.log(manager.execute(sendEmailCommand("Hi! I am Masud.", "128")));
+// console.log(manager.execute(sendEmailCommand("Hi! I am Masud.", "128")));
+// console.log(manager.execute(sendEmailCommand("Hi! I am Masud.", "128")));
+
+// console.log(manager.execute(trackEmailCommand("124")));
+// console.log(manager.execute(trackEmailCommand("1244")));
 
 console.log(manager.emails);
+
+// console.log(manager.execute(dispatchEmailCommand()));
+console.log(manager.execute(dispatchEmailByIdCommand("124", manager)));
+
+console.log(manager.emails);
+
+// take way:
+// - shallow copy vs deep copy
+// - function scope (global vs local)
+// - array pass by value vs pass by reference
+// - array -> splice, indexOf
