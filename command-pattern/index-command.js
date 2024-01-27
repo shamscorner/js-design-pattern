5732d540caa572ad17d4ef841fea2f848a1c4f29
Shamim Hossain <shossain@secureship.ca>
2024-01-27 12:45:51 +0600

    chore: revert back to send-email-command position

--- command-pattern/index-command.js
+++ command-pattern/index-command.js
@@ -1,4 +1,3 @@
-// file: EmailManager.js
 class EmailManager {
   constructor() {
     this.emails = []; // queue***
@@ -9,14 +8,12 @@ class EmailManager {
   }
 }
 
-// file: Command.js
 class Command {
   constructor(execute) {
     this.execute = execute;
   }
 }
 
-// file: utils.js
 function overflowEmail(length) {
   // limit 10
   const emailLimit = 5;
@@ -32,8 +29,7 @@ function overflowEmail(length) {
   };
 }
 
-// file: SendEmailCommand.js
-function sendEmailCommand(email, id) {
+function SendEmailCommand(email, id) {
   return new Command((emails) => {
     const { successful, message } = overflowEmail(emails.length);
 
@@ -44,74 +40,19 @@ function sendEmailCommand(email, id) {
   });
 }
 
-// file: TrackEmailCommand.js
-function trackEmailCommand(id) {
-  return new Command((emails) => {
-    const isEmailExist = emails.find((emailId) => emailId === id);
-
-    if (!isEmailExist)
-      return {
-        successful: false,
-        message: `Your email with the id - ${id} is not found!`,
-      };
-
-    return {
-      successful: true,
-      message: `Your email had sent a while ago with the id - ${id}.`,
-    };
-  });
-}
-
-// file: DispatchEmail.js
-function dispatchEmailCommand() {
-  return new Command((emails) => {
-    if (!emails.length) return `Your email queue is empty now.`;
-
-    const dispatchedEmailId = emails.shift();
-    return `Your email has been dispatched - ${dispatchedEmailId}`;
-  });
-}
-
-// file: DispatchEmail.js
-function dispatchEmailByIdCommand(id, manager) {
-  return new Command((emails) => {
-    const { successful, message } = manager.execute(trackEmailCommand(id));
-
-    if (!successful) return message;
-
-    const indexOfId = emails.indexOf(id);
-    emails.splice(indexOfId, 1);
-    return `Your email has been dispatched - ${id}`;
-  });
-}
-
 // For testing
 const manager = new EmailManager();
 
 console.log(manager.emails);
 
-console.log(manager.execute(sendEmailCommand("Hi! I am John.", "123")));
-console.log(manager.execute(sendEmailCommand("Hi! I am Alex.", "124")));
-console.log(manager.execute(sendEmailCommand("Hi! I am Macy.", "125")));
-// console.log(manager.execute(SendEmailCommand("Hi! I am Liza.", "126")));
-// console.log(manager.execute(SendEmailCommand("Hi! I am Sharif.", "127")));
-// console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
-// console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
-// console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
-// console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
+console.log(manager.execute(SendEmailCommand("Hi! I am John.", "123")));
+console.log(manager.execute(SendEmailCommand("Hi! I am Alex.", "124")));
+console.log(manager.execute(SendEmailCommand("Hi! I am Macy.", "125")));
+console.log(manager.execute(SendEmailCommand("Hi! I am Liza.", "126")));
+console.log(manager.execute(SendEmailCommand("Hi! I am Sharif.", "127")));
+console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
+console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
+console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
+console.log(manager.execute(SendEmailCommand("Hi! I am Masud.", "128")));
 
 console.log(manager.emails);
-
-// console.log(manager.execute(trackEmailCommand("123")));
-
-// console.log(manager.execute(trackEmailCommand("1234")));
-
-// console.log(manager.execute(dispatchEmailCommand()));
-console.log(manager.execute(dispatchEmailByIdCommand("125", manager)));
-
-console.log(manager.emails);
-
-// Take way:
-// - shallow copy vs deep copy
-// - splice, indexOf
-// - function scope (global vs local)
